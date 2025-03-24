adfb56eb25f3d3ac6d27fd6f1842518f5de30e89
nicolasromeroo <145489907+nicolasromeroo@users.noreply.github.com>
2025-03-24 04:33:13 -0300

    Update hashPassword.js

--- server/src/utils/hashPassword.js
+++ server/src/utils/hashPassword.js
@@ -2,9 +2,9 @@
 import bcrypt from "bcrypt";
 
 export const createHash = (password) => {
-  return bycrypt.hashSync(password, bycrypt.genSaltSync(10));
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 };
 
 export const isValidPassword = (password, userPassword) => {
-  return bycrypt.compareSync(password, userPassword); 
+  return bcrypt.compareSync(password, userPassword); 
 };
